8adf4f2753da4d808952defad0a745016f7a9654
Abdulbaasit-Seriki <baasisek01@gmail.com>
2020-05-11 13:38:04 +0100

    Added animation

--- index.js
+++ index.js
@@ -7,3 +7,20 @@ document.querySelector( "#nav-toggle" ).addEventListener( "click", function() {
         document.querySelector(".nav-content").style.display = "none";
     }
   });
+
+
+const debounceHelperFunc = (callback, delay = 500) => {
+	let timeOutId;
+	// It returns a function which serves as a shield or better still, a wrapper around the function
+	return (...args) => {
+		// If timeout is defined as in if time out has a value, clear the time out.
+		// This if statment prevents the function from saerching the aPI because it'll have cleared the interval 
+		// before 1000 milliseconds
+		if (timeOutId) {
+			clearTimeout(timeOutId);
+		}
+		timeOutId = setTimeout(() => {
+			callback.apply(null, args);
+		}, delay);
+	};
+}
