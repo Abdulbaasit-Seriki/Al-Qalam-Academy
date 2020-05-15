1ae725dae5966c492b953e75f23ec259c22c06e6
Abdulbaasit-Seriki <baasisek01@gmail.com>
2020-05-15 19:33:31 +0100

    Added a server

---
+++ server.js
@@ -0,0 +1,42 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+
+	let filePath = '.' + req.url;
+	if (filePath == './') {
+		filePath = './index.html';
+	}
+
+	const extName = path.extname(filePath);
+	let contentType = 'text/html';
+
+	switch (extName) {
+		case '.js': 
+			contentType = 'text/javascript';
+			break;
+		case '.css': 
+			contentType = 'text/css';
+			break;
+		case '.json': 
+				contentType = 'application/json';
+				break;
+	}
+
+	fs.readFile(filePath, (err, data) => {
+			if (err) {
+				res.writeHead(404);
+				res.write(`eroooor`);
+				res.end();
+			} else {
+				res.writeHead(200, {"Content-Type": contentType});
+				res.write(data, "utf-8");
+				res.end();
+			}
+	});
+});
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => console.log(`Server running on port ${port}`));
