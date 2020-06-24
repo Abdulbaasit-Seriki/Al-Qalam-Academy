99587e2a58afa59195cf7823d257213f35b494bc
Abdulbaasit <baasisek01@gmail.com>
2020-06-24 09:45:41 +0100

    fix: corrected the path for the css and js files

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
-const express = require('express');
-const app = express();
+const path = require('path')
+const express = require('express')
+const dotenv = require('dotenv')
+
+const connectToDB = require('./config/database')
+
+// Load Config files
+dotenv.config({ path: './config/config.env' })
+
+const app = express()
+// connectToDB()
 
 // Midddlewares
-app.use(express.static('public'));
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
 	res.sendFile('/index.html', { root: path.join(__dirname, './public') }, err => {
-		err ? next(err) : console.log(`File Sent`);
+		err ? next(err) : console.log(`File Sent`)
 	})
 })
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server running on ${port}`));
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`))
