7237b696270536f8bb6416385b70adc32520d9f6
Abdulbaasit <baasisek01@gmail.com>
2020-06-29 14:52:22 +0100

    fix: basic CRUD for class complete

--- models/Class.js
+++ models/Class.js
@@ -13,9 +13,3 @@ const ClassSchema = new mongoose.Schema({
 
 module.exports = mongoose.model('Class', ClassSchema)
 
-// PORT = 3000
-
-// GOOGLE_CLIENT_ID = 982690351452-9imebu2nuvgu0rmsvq8c65as4nagrjok.apps.googleusercontent.com
-// GOOGLE_CLIENT_SECRET = P9xc9QP6PCQBCI2myRgzY2Tk
-
-// MONGO_URI = mongodb+srv://Abdulbaasit:abdulbaasit@al-qalam-academy.hhsqo.mongodb.net/alqalam-academy?retryWrites=true&w=majority
