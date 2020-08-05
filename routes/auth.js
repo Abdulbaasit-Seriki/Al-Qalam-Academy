1e53af6fc627d2e464c4cfb107e583fbf9178a26
Abdulbaasit-Seriki <baasisek01@gmail.com>
2020-08-05 15:38:44 +0100

    feat: Dasboard design started

--- routes/auth.js
+++ routes/auth.js
@@ -76,7 +76,7 @@ router.post('/teachers/signup',
 	})
 
 	sendCookieToken(teacher, req)
-	res.redirect(`/teachers/${teacher.id}`)
+	res.render('dashboard')
 }))
 
 
@@ -96,7 +96,7 @@ router.post('/teachers/login', [validatePassword, checkUserExistence],
 	const teacher =  await Teacher.findOne({ displayName: req.body.displayName })
 	sendCookieToken(teacher, req)
 
-	res.redirect(`/teachers/${teacher.id}`)
+	res.render('dashboard')
 }))
 
 // description     	Forgot Password
